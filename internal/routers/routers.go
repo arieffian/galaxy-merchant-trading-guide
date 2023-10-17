e240d525b3992453c881f2ae7db70827ac76f7b9
Donny Arieffian <donny.arieffian@gmail.com>
2023-10-17 12:19:25 +0700

    feat: init project with personal boilerplate

---
+++ internal/routers/routers.go
@@ -0,0 +1,45 @@
+package routers
+
+import (
+	"github.com/arieffian/roman-alien-currency/internal/handlers"
+	"github.com/arieffian/roman-alien-currency/internal/pkg/redis"
+	userRepository "github.com/arieffian/roman-alien-currency/internal/repositories/users"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+type Router struct {
+	healthcheck handlers.HealthcheckService
+	users       handlers.UserService
+}
+
+type NewRouterParams struct {
+	Db    *gorm.DB
+	Redis redis.RedisService
+}
+
+func NewRouter(p NewRouterParams) (*Router, error) {
+	userRepo := userRepository.NewUserRepository(userRepository.NewUserRepositoryParams{
+		Db:    p.Db,
+		Redis: p.Redis,
+	})
+
+	healthcheckHandler := handlers.NewHealthCheckHandler()
+	userHandler := handlers.NewUserHandler(handlers.NewUserHandlerParams{
+		UserRepo: userRepo,
+	})
+
+	return &Router{
+		healthcheck: healthcheckHandler,
+		users:       userHandler,
+	}, nil
+}
+
+func (r *Router) RegisterRoutes(routes *fiber.App) {
+	v1 := routes.Group("/api/v1")
+	v1.Get("/healthcheck", r.healthcheck.HealthCheckHandler)
+
+	users := v1.Group("/users")
+	users.Get("/", r.users.ListUsers)
+	users.Get("/:id", r.users.GetUserById)
+}
